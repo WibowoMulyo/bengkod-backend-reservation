476f902dae0350b41af34b377e52bc956d095cfe
WibowoMulyo <111202214787@mhs.dinus.ac.id>
2024-10-16 13:59:46 +0700

    Add middleware to /logout

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', action: [AuthController::class, 'register']);
 Route::post('/login', action: [AuthController::class, 'login']);
-Route::post('/logout', action: [AuthController::class, 'logout']);
+Route::post('/logout', action: [AuthController::class, 'logout'])->middleware('auth:api');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
