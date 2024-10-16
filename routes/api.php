312ae2869db1373082cd7de92262475422b0248c
WibowoMulyo <111202214787@mhs.dinus.ac.id>
2024-10-16 11:04:04 +0700

    Resolve conflict

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,4 @@ Route::post('/logout', action: [AuthController::class, 'logout']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
->>>>>>> main
 });
